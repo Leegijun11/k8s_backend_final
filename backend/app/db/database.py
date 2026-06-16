93df764dd2d1b7701d0a85577cdbe8e2b71dd49d
Leegijun <dlrlwms11@gmail.com>
2026-06-16 23:55:11 +0900

    test 6

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -2,7 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 from app.core.config import settings
 
-engine = create_engine(settings.DATABASE_URL)
+
+db_url = settings.DATABASE_URL
+if db_url.startswith("mysql://"):
+    db_url = db_url.replace("mysql://", "mysql+pymysql://")
+
+
+engine = create_engine(db_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
