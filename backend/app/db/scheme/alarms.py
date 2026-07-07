0adcfd56e51c2e0374c08156ac328b44ea3fe1b1
jonghoon <ljh11442@gmail.com>
2026-07-07 17:05:46 +0900

    Edit app/db/scheme/alarms.py

--- backend/app/db/scheme/alarms.py
+++ backend/app/db/scheme/alarms.py
@@ -1,12 +1,13 @@
-from pydantic import BaseModel, Field, EmailStr, ConfigDict
-from datetime import datetime,timezone
-from typing import Annotated
+from pydantic import BaseModel, Field, ConfigDict
+from datetime import datetime, timezone
+from typing import Optional
 
 
-class Alarm_Base(BaseModel): 
-    send_id: int
+class Alarm_Base(BaseModel):
+    send_id: Optional[int] = None  
     receive_id: int
-    g_id: int
+    g_id: Optional[int] = None      
+    a_type: str = "invite"          
 
     model_config = ConfigDict(from_attributes=True)
 
@@ -14,7 +15,9 @@ class Alarm_Base(BaseModel):
 class Alarm_Create(Alarm_Base):
     pass
 
-    
+
 class Alarm_Read(Alarm_Base):
     a_id: int
-    a_created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    a_created_at: datetime = Field(
+        default_factory=lambda: datetime.now(timezone.utc)
+    )
